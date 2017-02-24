4b683f6d21770fe7be02ceb1ab1bb231fa01102a
Mikkel Ricky <rimi@aarhus.dk>
2017-02-24 04:58:27 +0100

    Fixed hacks

--- assets/js/node/add/site.js
+++ assets/js/node/add/site.js
@@ -1,29 +1,30 @@
 ;(function($) {
-		$(document).ready(function() {
-				var id;
+    $(document).ready(function() {
+        var id;
 
-				var masterDomain = function(url) {
-						var match = new RegExp("://([^/]+)/").exec(url);
-						return match[1];
-				}(document.location.href);
+        if (!$('#edit-title').val()) {
+            var masterDomain = function(url) {
+                var match = new RegExp("://([^/]+)/").exec(url);
+                return match[1];
+            }(document.location.href);
 
-				$('#edit-title').val('.' + masterDomain);
-				// $('#edit-client').val('Aarhus kommune');
+            $('#edit-title').val('.' + masterDomain);
+            $('#edit-title').focus();
+            try {
+                document.getElementById('edit-title').setSelectionRange(0, 0);
+            } catch (ex) {}
+        }
+        $('#edit-client').val('Aarhus kommune');
 
-				id = $('label[for^="edit-profile"]:contains("ITKore")').attr('for');
-				$('#' + id).attr('checked', 'checked');
+        id = $('label[for^="edit-profile"]:contains("aarhus.dk")').attr('for');
+        $('#' + id).attr('checked', 'checked');
 
-				id = $('label[for^="edit-platform"]:contains("ITKore")').attr('for');
-				$('#' + id).attr('checked', 'checked');
+        id = $('label[for^="edit-platform"]:contains("aarhus.dk")').attr('for');
+        $('#' + id).attr('checked', 'checked');
 
-				id = $('label[for^="edit-site-language"]:contains("Danish")').attr('for');
-				$('#' + id).attr('checked', 'checked');
+        id = $('label[for^="edit-site-language"]:contains("Danish")').attr('for');
+        $('#' + id).attr('checked', 'checked');
 
-				$('#hosting-site-field-client, #hosting-site-field-profile, #hosting-site-field-platform, #hosting-site-field-site-language, #hosting-site-field-db-server').hide();
-
-				$('#edit-title').focus();
-				try {
-						document.getElementById('edit-title').setSelectionRange(0, 0);
-				} catch (ex) {}
-});
+        $('#hosting-site-field-client, #hosting-site-field-profile, #hosting-site-field-platform, #hosting-site-field-site-language, #hosting-site-field-db-server').hide();
+    });
 }(jQuery));
